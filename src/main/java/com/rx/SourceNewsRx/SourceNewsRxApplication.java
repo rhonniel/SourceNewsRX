ab858cc40b3cd14347e70b018b0c566f2a605675
rhonniel <rhonniel777@gmail.com>
2025-04-15 21:36:53 -0400

    init

---
+++ src/main/java/com/rx/SourceNewsRx/SourceNewsRxApplication.java
@@ -0,0 +1,13 @@
+package com.rx.SourceNewsRx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceNewsRxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceNewsRxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rx/SourceNewsRx/SourceNewsRxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rx.SourceNewsRx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceNewsRxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
